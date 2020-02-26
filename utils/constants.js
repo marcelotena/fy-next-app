7ccee09e6e0d0e7d79fb0413c79769ea7be6afad
Marcelo Tena <marcelotena@fastandyours.com>
2020-02-26 09:13:23 +0100

    Test with local WP

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
-export const DOMAIN_URL = 'https://back.fastandyours.com';
+//export const DOMAIN_URL = 'https://back.fastandyours.com';
+export const DOMAIN_URL = 'http://fy-agency-site.test';
 export const WP_REST_API = `/wp-json/wp/v2`;
 export const FY_CUSTOM_API = `/wp-json/fy/v1`;

c1f736d0cf83c13fc223ace76f9a0f079b284cd4
Marcelo Tena <marcelotena@fastandyours.com>
2020-05-15 18:51:01 +0200

    Only fetch invoices if user is logged in

--- components/dashboard/InvoiceList.js
+++ components/dashboard/InvoiceList.js
@@ -3,15 +3,18 @@ import { connect } from 'react-redux';
 import { fetchInvoices } from '../../actions';
 
 import InvoiceListTable from "./InvoiceListTable";
+import PropTypes from "prop-types";
 
-const InvoiceList = ({ fetchInvoices }) => {
+const InvoiceList = ({ fetchInvoices, invoices, isAuthenticated }) => {
 
   useEffect(() => {
-    fetchInvoices()
+    if(isAuthenticated) {
+      fetchInvoices();
+    }
   }, []);
 
-  const renderList = (invoices) => {
-    return <InvoiceListTable invoices={[]} />;
+  const renderList = () => {
+    return <InvoiceListTable invoices={invoices} />;
   };
 
   return (
@@ -25,8 +28,17 @@ const InvoiceList = ({ fetchInvoices }) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return { invoices: state.invoices}
+InvoiceList.propTypes = {
+  fetchInvoices: PropTypes.func.isRequired,
+  invoices: PropTypes.array,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+  return {
+    invoices: state.invoices,
+    isAuthenticated: state.auth.isAuthenticated
+  }
 };
 
 export default connect(mapStateToProps, { fetchInvoices })(InvoiceList);
